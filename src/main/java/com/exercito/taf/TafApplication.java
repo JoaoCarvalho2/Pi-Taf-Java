6cd8126d3abaf744a0c154dbfacce0b2110cab60
João Victor Barbosa <shddml@hotmail.com>
2026-01-04 02:25:47 -0300

    feat: add main application class

---
+++ src/main/java/com/exercito/taf/TafApplication.java
@@ -0,0 +1,12 @@
+package com.exercito.taf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TafApplication.class, args);
+    }
+}
